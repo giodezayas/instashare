cfb6824254656675a0d4d2cc0e87077ea2630e55
giovanni <gdzayas@gmail.com>
2024-03-21 15:22:40 -0400

    Configurando estructura y Componentes para cliente

---
+++ client/src/lang/homepage.js
@@ -0,0 +1,6 @@
+export const HomepageMsg = {
+  msgHomepage: "Welcome to InstaShare",
+  msgDescription:
+    "Share your files securely and easily with our file hosting service.",
+  msgUploadNow: "Upload Now",
+};
